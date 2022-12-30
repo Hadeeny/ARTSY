d0a9949a2129747dd33a0961e0373fc1a89a604d
hadeeny <oluwasegunadeniyi064@gmail.com>
2022-12-30 03:53:00 +0100

    almost

--- src/pages/Livebid.jsx
+++ src/pages/Livebid.jsx
@@ -86,7 +86,7 @@ const Livebid = ({ selectedId, auction, handleClose, data }) => {
       </motion.section>
       {/* Mobile */}
       <motion.section
-        initial={{ opacity: 0, scale: 0 }}
+        initial={{ opacity: 0, scale: 0 }} 
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 1, scale: 0 }}
         transition={{ duration: 0.4 }}
